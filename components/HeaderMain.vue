821251dc9a053f78557f225e814bf502dd7cbb7e
Kevyn lucca <Kevynalternativo@gmail.com>
2025-03-02 21:57:52 -0300

    @kevyn-lucca || Adicionado front da login page

--- components/HeaderMain.vue
+++ components/HeaderMain.vue
@@ -1,44 +1,42 @@
 <template>
-	<nav class="backgroundhead">
-		<div class="flex p-4 place-content-between items-center h-20">
-			<div class="flex place-content-between w-2/5">
-				<div>
-					<img src="assets/bread-svgrepo-com(4).svg" alt="Logo" class="w-20 h-20" />
-				</div>
-				<div id="headerMain" class="flex-1 mt-4">
-					<ul v-if="false" class="flex gap-6 justify-center items-center relative">
-						<li>
-							<button class="buttonClassico"><a href="#">Sobre</a></button>
-						</li>
-						<li>
-							<button class="buttonClassico"><a href="#">Cadastro</a></button>
-						</li>
-					</ul>
-					<ul v-else></ul>
-				</div>
-			</div>
-
-			<div v-if="false" class="flex w-20 items-center gap-2">
-				<a aria-label="Log-out" href="#">
-					<svg class="w-8 h-8 mt-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-						<path
-							fill="#d98b4d"
-							d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z"
-						/>
-					</svg>
-				</a>
-			</div>
-			<div v-else>
-				<button class="buttonClassico"><a href="#">Login</a></button>
-			</div>
-		</div>
-	</nav>
+  <nav class="backgroundhead">
+    <div class="flex p-4 place-content-between items-center h-20">
+      <div class="flex place-content-between w-2/5">
+        <div>
+          <img
+            src="assets/bread-svgrepo-com(4).svg"
+            alt="Logo"
+            class="w-20 h-20"
+          />
+        </div>
+      </div>
+      <div v-if="false" class="flex w-20 items-center gap-2">
+        <a aria-label="Log-out" href="#">
+          <svg
+            class="w-8 h-8 mt-1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <path
+              fill="#d98b4d"
+              d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z"
+            />
+          </svg>
+        </a>
+      </div>
+      <div v-else>
+        <NuxtLink to="/login">
+          <button class="buttonClassico">Login</button>
+        </NuxtLink>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <style scoped lang="scss">
 .backgroundhead {
-	background-color: $HeaderBgMain;
-	color: $ConteudoTheme;
+  background-color: $HeaderBgMain;
+  color: $ConteudoTheme;
 }
 </style>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,81 +1,71 @@
 <template>
-	<div
-		class="relative min-h-[40rem] lg:min-h-0 bg-surface-0 dark:bg-primary-600 flex lg:flex-row flex-col"
-	>
-		<div class="flex lg:flex lg:flex-row flex-col justify-center md:justify-normal h-full flex-1">
-			<div class="relative flex-1 z-20 flex items-center justify-center">
-				<div class="flex items-center justify-center h-full">
-					<div class="w-full max-w-2xl px-6 py-12 lg:p-12 xl:p-16 text-center lg:text-left">
-						<h1
-							class="text-4xl xl:text-5xl font-bold text-surface-0 lg:text-surface-900 dark:text-surface-0 mb-6 !leading-tight"
-						>
-							<span class="block">O sistema que seu </span>
-							<span class="block text-primary">
-								negocio
-								<span id="mudaPalavra">precisa</span></span
-							>
-						</h1>
-						<p
-							class="text-surface-0/90 lg:text-surface-700 dark:text-surface-200 text-xl leading-normal mb-8 max-w-xl lg:max-w-none"
-						>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-							incididunt ut labore et dolore magna aliqua.
-						</p>
+  <div
+    class="relative min-h-[40rem] lg:min-h-0 bg-surface-0 dark:bg-primary-600 flex lg:flex-row flex-col"
+  >
+    <div
+      class="flex lg:flex lg:flex-row flex-col justify-center md:justify-normal h-full flex-1"
+    >
+      <div class="relative flex-1 z-20 flex items-center justify-center">
+        <div class="flex items-center justify-center h-full">
+          <div
+            class="w-full max-w-2xl px-6 py-12 lg:p-12 xl:p-16 text-center lg:text-left"
+          >
+            <h1
+              class="text-4xl xl:text-5xl font-bold text-surface-0 lg:text-surface-900 dark:text-surface-0 mb-6 !leading-tight"
+            >
+              O sistema que seu negocio
+              <span aria-label="precisa" class="typewriter"></span>
+            </h1>
+            <p
+              class="text-surface-0/90 lg:text-surface-700 dark:text-surface-200 text-xl leading-normal mb-8 max-w-xl lg:max-w-none"
+            >
+              Um sistema automatizado e inteligente de organização de estoque
+              para padarias. é a solução ideal para transformar a rotina da sua
+              padaria, promovendo maior organização, redução de desperdícios e
+              aumentando significativamente a eficiência no processo de
+              gerenciamento de produtos.
+            </p>
 
-						<div class="flex items-center gap-4 justify-center lg:justify-start">
-							<button
-								class="buttonClassico !border-primary-600 hover:border-none !text-white hover:!text-black hover:!bg-green-800 !bg-green-400"
-							>
-								<a href="#">Cadastre-se</a>
-							</button>
-							<button
-								class="buttonClassico !border-primary-600 hover:border-none !text-white hover:!text-black hover:!bg-green-800 !bg-green-400"
-							>
-								<a href="#">Saiba mais</a>
-							</button>
-						</div>
-					</div>
-				</div>
-			</div>
+            <div
+              class="flex items-center gap-4 justify-center lg:justify-start"
+            >
+              <button
+                class="buttonClassico !border-primary-600 hover:border-none !text-white hover:!text-black hover:!bg-green-800 !bg-green-400"
+              >
+                <a href="#">Cadastre-se</a>
+              </button>
+              <button
+                class="buttonClassico !border-primary-600 hover:border-none !text-white hover:!text-black hover:!bg-green-800 !bg-green-400"
+              >
+                <a href="#">Saiba mais</a>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
 
-			<div class="absolute lg:relative inset-0 lg:inset-auto flex-1">
-				<div class="absolute lg:hidden inset-0 bg-surface-900/60 dark:bg-surface-900/80 z-10" />
-				<img
-					src="https://fqjltiegiezfetthbags.supabase.co/storage/v1/render/image/public/block.images/blocks/hero/hero-1.png"
-					alt="hero-1"
-					class="h-full w-full object-cover xl:[clip-path:polygon(25%_0,100%_0%,100%_100%,0_100%)]"
-				/>
-			</div>
-		</div>
-	</div>
+      <div class="absolute lg:relative inset-0 lg:inset-auto flex-1">
+        <div
+          class="absolute lg:hidden inset-0 bg-surface-900/60 dark:bg-surface-900/80 z-10"
+        />
+        <img
+          src="https://fqjltiegiezfetthbags.supabase.co/storage/v1/render/image/public/block.images/blocks/hero/hero-1.png"
+          alt="hero-1"
+          class="h-full w-full object-cover xl:[clip-path:polygon(25%_0,100%_0%,100%_100%,0_100%)]"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="css" scoped>
 .buttonClassico:hover {
-	box-shadow: 0.25rem 0.25rem black, 0 0 10px rgba(0, 0, 0, 0.2) !important;
+  box-shadow: 0.25rem 0.25rem black, 0 0 10px rgba(0, 0, 0, 0.2) !important;
 }
 </style>
 
 <script setup>
 definePageMeta({
-	layout: 'prelogin',
-});
-//loppa pelo array de palavras e
-let Arraypalavras = ['quer', 'necessita', 'ama', 'adora'];
-let num = 0;
-onMounted(() => {
-	const mudaPalavra = (num) => {
-		const palavra = Arraypalavras[num];
-		const elementoPalavra = document.querySelector('#mudaPalavra');
-		elementoPalavra.innerHTML = palavra;
-	};
-
-	setInterval(() => {
-		if (num >= Arraypalavras.length) {
-			num = 0;
-		}
-		mudaPalavra(num);
-		num++;
-	}, 4000);
+  layout: "prelogin",
 });
 </script>
