4bf9ac1e391e5cf1c3d232577f54693643318adb
AlexandreAO-0 <lendefeisehotmail@gmail.com>
2025-05-07 10:21:44 -0300

    @AnaGabriely || componente itemtarefa criado

---
+++ components/tarefaBlock.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div class= "itemtarefa">
+            <h1>Para fazer</h1>
+            <UBadge>teste</UBadge>
+        </div>
+    </div>
+</template>
+
+
+<style  lang="css" scoped>
+.itemtarefa{
+    display: flex;
+}
+</style>
---
+++ pages/tarefas/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        teste
+        <tarefaBlock></tarefaBlock>
+    </div>
+</template>
+
