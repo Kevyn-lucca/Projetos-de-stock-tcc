8ea7f91141b4f817d23059955ab310d16bb5435b
AnaGabriely <anyy.213@hotmail.com>
2025-10-08 14:57:29 -0300

    @AnaGabriely || página sobre criada

---
+++ pages/sobre/index.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="flex top-0 h-screen flex items-center justify-center flex-col place-content-center w-auto gap-5 text-lg text-(length:--my-text-size)">
+<h1 class="w-30 text-2xl">
+    Sobre nós 
+
+</h1>
+<p class="w-3xl">
+    Somos uma empresa de controle de estoque no ramo de panificadoras, 
+    temos o compromentimento de entregar usabilidade, performace, eficiência,
+    segurança e uma interface intuitiva nosso foco principal no gerenciamento de estoque
+    e na prevenção de perdas com um sistema que controla a validade, quantidade, 
+    e a produtividade dos colaboradores sendo assim garantindo um alto desemprenho administrativo.
+    
+</p>
+<img src="assets/hamide-jafari-974x2guatCs-unsplash.jpg" alt="imagem de um padeiro fazendo pão, Hamide Jafari" class="w-48">
+</div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "prelogin" 
+});
+</script>
