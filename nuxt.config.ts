afa9d243b715d8759f5524e9b70e041deba32436
AnaGabriely <143809806+AnaGabriely@users.noreply.github.com>
2025-10-20 17:18:23 -0300

    Add security headers configuration to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,14 +36,14 @@ export default defineNuxtConfig({
     "nuxt-security",
   ],
 
+export default defineNuxtConfig({
   security: {
     headers: {
       contentSecurityPolicy: {
         "default-src": ["'self'"],
         "img-src": ["'self'", "data:", "https://api.dicebear.com"],
         "connect-src": ["'self'", "http://localhost:8080", "https://localhost:8080"],
-
       },
     },
   },
-});
+})
